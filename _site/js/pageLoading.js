b34a06a049814cd3882862ec7a0c5e1b282ee88f
殷成涛 <yinwoods@163.com>
2015-04-26 12:48:18 +0800

    改善文章页面

--- _site/js/pageLoading.js
+++ _site/js/pageLoading.js
@@ -1,4 +1,4 @@
-/* 
+/*
 * @Author: 卓文理 www.zwlme.com
 * @Email:  531840344@qq.com
 * @Date:   2014-11-26 13:34:04
@@ -16,7 +16,7 @@
 		classie.add(container,'loading');
 		startLoading();
 		// 3d字体
-		console.log("%c卓文理"," text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:20em;color:#fff;");
+		console.log("%cyinwoods"," text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:20em;color:#fff;");
 	}
 
 	function startLoading() {
